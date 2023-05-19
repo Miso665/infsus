81b0d76ad6d7189e894a190c7b6a7022e079ebf2
vbedekovic16 <vb52325@fer.hr>
2023-05-19 10:12:31 +0200

    Allow react app to use api

--- backend/projekt/src/index.ts
+++ backend/projekt/src/index.ts
@@ -10,6 +10,27 @@ import { dbGetBrand } from './db/brandDBAccess';
 import { BrandDTO } from './models/brand';
 import { dbGetAllModels } from './db/modelDBAccess';
 
+
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', 1);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+
 import apiRoutes from "./routes/api/apiRoutes";
 app.use("/api", apiRoutes);
 
