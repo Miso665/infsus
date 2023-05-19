287142de63b27966e23ab3c2d53889e167a40b9c
vbedekovic16 <vb52325@fer.hr>
2023-05-19 10:16:08 +0200

    fix2

--- backend/projekt/src/index.ts
+++ backend/projekt/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { QueryResult } from 'pg';
-
+var cors = require('cors');
 
 const app = express();
 const port = 8080;
@@ -11,25 +11,7 @@ import { BrandDTO } from './models/brand';
 import { dbGetAllModels } from './db/modelDBAccess';
 
 
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', 1);
-
-  // Pass to next layer of middleware
-  next();
-});
-
+app.use(cors({origin: 'http://127.0.0.1:3000'}));
 
 import apiRoutes from "./routes/api/apiRoutes";
 app.use("/api", apiRoutes);
