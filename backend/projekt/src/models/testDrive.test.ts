88450c94a1277750ef387f94ed5e1d097ce1a7d5
vbedekovic16 <vb52325@fer.hr>
2023-05-24 11:16:58 +0200

    Test date text fix

--- backend/projekt/src/models/testDrive.test.ts
+++ backend/projekt/src/models/testDrive.test.ts
@@ -14,7 +14,7 @@ test('TestDrive must be invalid on testdriveconcluded and userid beacuse of miss
 test('TestDrive must be invalid beacuse composite key already exists', async () => {
     expect(await TestDriveDTO.validate(
         {
-            "testdrivetime": "2023-07-04T10:00:00.000Z",
+            "testdrivetime": "2023-07-04T12:00:00.000Z",
             "testdriveconcluded": true,
             "testdriveid": 1,
             "userid": 3,
